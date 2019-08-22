a2ec5d4660fa21c98e3634fbdd0dd30a42203168
Sergey Starodubov <4analit@gmail.com>
2019-08-22 08:43:39 +0400

    getText update with select tag

--- getText.js
+++ getText.js
@@ -1,26 +1,49 @@
 module.exports = {
   runTest: async function(args) {
-    const { page, selectors, data, log, options, helper, levelIndent, _ } = args;
-    let selector = helper.anyGet(selectors, 'selector');
-    let screenshot = _.get(options, 'screenshot', false);
-    let attr = helper.anyGet(data, 'attribute');
-    const element = await helper.getElement(page, selector);
+    let selector, screenshot, element, attr, text, value, selectorSelect, elementSelect;
+    try {
+      const { page, selectors, data, log, options, helper, levelIndent, _ } = args;
+      selector = helper.anyGet(selectors, 'selector');
+      screenshot = _.get(options, 'screenshot', false);
+      attr = helper.anyGet(data, 'attribute');
+      element = await helper.getElement(page, selector);
 
-    let text = await page.evaluate(element => {
-      return element.innerText || element.value;
-    }, element);
+      if (element) {
 
-    await log({
-      text: `Получен атрибут текст '${text}' на селекторе = ${selector}`,
-      screenshot: screenshot,
-      fullpage: false,
-      element: element,
-      level: 'debug',
-      levelIndent: levelIndent + 1,
-    });
+        text = await page.evaluate(element => {
+          return element.innerText;
+        }, element);
+        value = await page.evaluate(element => {
+          return element.value;
+        }, element);
 
-    return {
-      text: text,
-    };
+        // Select fetch data
+        if (value && !text) {
+          selectorSelect = selector + ` > option[value = "${value}"]`
+          elementSelect = await helper.getElement(page, selectorSelect);
+          text = await page.evaluate(element => {
+            return element.label;
+          }, elementSelect);
+        }
+      } else {
+        throw { message: `Can't find selector ${selector}` };
+      }
+
+      await log({
+        text: `Get text: '${text}' from selector: '${selector}'`,
+        screenshot: screenshot,
+        fullpage: false,
+        element: element,
+        level: 'debug',
+        levelIndent: levelIndent + 1,
+      });
+
+      return { text, value };
+    } catch (error) {
+      error.testType = 'atom';
+      error.testArgs = args;
+      error.testVars = { selector, screenshot, element, attr, text, value, selectorSelect, elementSelect };
+      throw error;
+    }
   },
 };
