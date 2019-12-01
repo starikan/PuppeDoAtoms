3598aa8a12eb0146303f9976ff9112cf23a7a49a
Sergey Starodubov <4analit@gmail.com>
2019-12-01 20:02:59 +0400

    Click Selector

--- clickSelector.js
+++ clickSelector.js
@@ -3,7 +3,7 @@ module.exports = {
     const { page, selectors, log, options, helper, levelIndent } = args;
     const selector = helper.anyGet(selectors, 'selector');
     const element = await helper.getElement(page, selector);
-    const { screenshot = false, fullpage = false, count = 1, delay = 0, button = 'left' } = options;
+    const { screenshot = false, fullpage = false, count = 1, delay = 1, button = 'left' } = options;
 
     await log({
       text: `Нажат селектор = ${selector}`,
@@ -14,6 +14,8 @@ module.exports = {
       levelIndent: levelIndent + 1,
     });
 
-    await element.click({ clickCount: count, delay, button });
+    for (let i=0; i < count; i++) {
+      await element.click({ delay, button });
+    }
   },
 };
