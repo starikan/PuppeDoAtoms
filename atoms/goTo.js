304f46f3b24f3a0d9157dad413e6f2bbb2803be5
Sergey Starodubov <4analit@gmail.com>
2020-01-03 14:59:44 +0400

    goTo refactoring

--- atoms/goTo.js
+++ atoms/goTo.js
@@ -7,17 +7,3 @@ instance.atomRun = async function() {
   await this.page.goto(url);
   this.log({ text: `Go to: ${url}` });
 };
-
-// module.exports = {
-//   runTest: async function(args) {
-//     const { page, data, log, levelIndent } = args;
-//     await page.goto(data.url);
-//     await log({
-//       text: `Go to: ${data.url}`,
-//       level: 'info',
-//       screenshot: false,
-//       fullpage: false,
-//       levelIndent: levelIndent + 1,
-//     });
-//   },
-// };
--- atoms/waitTime.js
+++ atoms/waitTime.js
@@ -7,20 +7,3 @@ instance.atomRun = async function() {
   this.log({ text: `Waiting ${time} ms.` });
   await this.page.waitFor(time);
 };
-
-// module.exports = {
-//   runTest: async function(args) {
-//     const { page, data, log, levelIndent } = args;
-//     const time = data.time;
-
-//     await page.waitFor(time);
-
-//     await log({
-//       text: `waitTime ${time}ms`,
-//       screenshot: false,
-//       fullpage: false,
-//       level: 'debug',
-//       levelIndent: levelIndent + 1,
-//     });
-//   },
-// };
