207b89734c1aedc69a1feb41b62c9da2aee16052
Sergey Starodubov <4analit@gmail.com>
2020-01-30 16:59:27 +0400

    typeInput fix

--- main/typeInput.js
+++ main/typeInput.js
@@ -6,27 +6,7 @@ instance.atomRun = async function() {
   const { input } = this.selectors;
   const { text } = this.data;
   const element = await this.getElement(this.page, input);
-  await element.type(text);
+  await element.type(String(text));
 
   this.log({ text: `Type in selector: '${input}', text: '${text}'`, element });
 };
-
-// module.exports = {
-//   runTest: async function(args) {
-//     const { page, data, selectors, options, log, helper, levelIndent, _ } = args;
-//     const selector = selectors.input;
-//     const screenshot = _.get(options, 'screenshot', false);
-//     const text = data.text;
-//     const element = await helper.getElement(page, selector);
-//     await element.type(text);
-
-//     await log({
-//       text: `Ввод текста в INPUT = ${selector}, TEXT = ${text}`,
-//       screenshot: screenshot,
-//       fullpage: false,
-//       element: element,
-//       level: 'debug',
-//       levelIndent: levelIndent + 1,
-//     });
-//   },
-// };
