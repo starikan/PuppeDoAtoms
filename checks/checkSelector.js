078385aee237f520cf23134707809ba4b334c9d4
Sergey Starodubov <4analit@gmail.com>
2020-03-05 13:29:47 +0400

    checkSelector

--- checks/checkSelector.js
+++ checks/checkSelector.js
@@ -6,49 +6,11 @@ instance.atomRun = async function() {
   const { selector } = this.selectors;
   const element = await this.getElement(this.page, selector);
 
-  await this.log({ text: `Check selector exist: '${selector}'` });
-
   if (element) {
-    await this.log({ text: `Селектор найден = ${selector}` });
+    await this.log({ text: `Check selector exist: '${selector}'` });
   } else {
-    await this.log({ text: `Селектор НЕ найден = ${selector}`, level: 'error' });
+    await this.log({ text: `Check selector NOT exist: '${selector}'` });
   }
+
   return { exists: !!element };
 };
-
-// module.exports = {
-//   runTest: async function(args) {
-//     const { page, selectors, log, helper, levelIndent } = args;
-//     const selector = helper.anyGet(selectors, 'selector');
-//     const element = await helper.getElement(page, selector);
-
-//     await log({
-//       text: `Проверка наличия селектора = ${selectors.selector}`,
-//       screenshot: false,
-//       level: 'raw',
-//       levelIndent: levelIndent + 1,
-//     });
-
-//     if (element) {
-//       await log({
-//         text: `Селектор найден = ${selector}`,
-//         screenshot: false,
-//         level: 'raw',
-//         levelIndent: levelIndent + 1,
-//       });
-//       return {
-//         exists: true,
-//       };
-//     } else {
-//       await log({
-//         text: `Селектор НЕ найден = ${selector}`,
-//         screenshot: false,
-//         level: 'raw',
-//         levelIndent: levelIndent + 1,
-//       });
-//       return {
-//         exists: false,
-//       };
-//     }
-//   },
-// };
