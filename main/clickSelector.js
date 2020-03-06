c2ebbea2a80bcc05c23b2f4d419ce9e7e473cd74
Sergey Starodubov <4analit@gmail.com>
2020-03-06 12:21:19 +0400

    clickSelector

--- main/clickSelector.js
+++ main/clickSelector.js
@@ -4,13 +4,18 @@ module.exports = { runTest: instance.runTest.bind(instance) };
 // WRITE YOUR LOGIC BELLOW
 instance.atomRun = async function() {
   const { selector } = this.selectors;
-  const { count = 1, delay = 1, button = 'left' } = this.options;
+  const { count = 1, delay = 1, button = 'left', logAfter = false } = this.options;
   const element = await this.getElement(this.page, selector);
 
   for (let i = 0; i < count; i++) {
     try {
+      if (!logAfter) {
+        await this.log({ text: `Click selector: '${selector}'`, element });
+      }
       await element.click({ delay, button });
-      await this.log({ text: `Click selector: '${selector}'`, element });
+      if (logAfter) {
+        await this.log({ text: `Click selector: '${selector}'`, element });
+      }
     } catch (error) {
       await this.log({
         text: `Can't click selector: '${selector}'`,
