4d3efeb583a90810d51a87e80b1ef2bad2213246
Sergey Starodubov <4analit@gmail.com>
2020-09-06 22:51:50 +0400

    Rethinking getText

--- main/getText.js
+++ main/getText.js
@@ -2,12 +2,15 @@ const instance = new (require('@puppedo/atoms-core'))();
 module.exports = { runTest: instance.runTest.bind(instance) };
 
 // WRITE YOUR LOGIC BELLOW
-instance.atomRun = async function() {
+instance.atomRun = async function () {
   const { selector } = this.selectors;
-  const element = await this.getElement(selector);
+  const { selectorNumber } = this.data;
+
+  const elements = await this.getElement(selector, true);
+  const element = elements[selectorNumber || 0];
 
   if (element) {
-    const { tagName, innerText, value } = await this.page.evaluate(element => {
+    const { tagName, innerText, value } = await this.page.evaluate((element) => {
       return { tagName: element.tagName, innerText: element.innerText, value: element.value };
     }, element);
 
@@ -15,9 +18,9 @@ instance.atomRun = async function() {
 
     // Select fetch data
     if (value && !text && tagName === 'SELECT') {
-      const selectorSelect = `${selector} > option[value = "${value}"]`;
-      const elementSelect = await this.getElement(selectorSelect);
-      text = await this.page.evaluate(element => element.label, elementSelect);
+      const selectorSelect = `option[value = "${value}"]`;
+      const elementSelect = await this.getElement(selectorSelect, false, element);
+      text = await this.page.evaluate((element) => element.label, elementSelect);
     }
 
     // Input
@@ -29,58 +32,6 @@ instance.atomRun = async function() {
 
     return { text };
   } else {
-    throw { message: `Can't find selector: '${selector}'` };
+    throw { message: `Can't find selector: '${selector}' by index ${selectorNumber || 0}` };
   }
 };
-
-// module.exports = {
-//   runTest: async function(args) {
-//     let selector, screenshot, element, attr, text, value, selectorSelect, elementSelect, tagName;
-//     try {
-//       const { page, selectors, data, log, options, helper, levelIndent, _ } = args;
-//       selector = helper.anyGet(selectors, 'selector');
-//       screenshot = _.get(options, 'screenshot', false);
-//       attr = helper.anyGet(data, 'attribute');
-//       element = await helper.getElement(page, selector);
-
-//       if (element) {
-//         ({ tagName, text, value } = await page.evaluate(element => {
-//           return { tagName: element.tagName, text: element.innerText, value: element.value };
-//         }, element));
-
-//         // Select fetch data
-//         if (value && !text && tagName === 'SELECT') {
-//           selectorSelect = selector + ` > option[value = "${value}"]`;
-//           elementSelect = await helper.getElement(page, selectorSelect);
-//           text = await page.evaluate(element => {
-//             return element.label;
-//           }, elementSelect);
-//         }
-
-//         // Input
-//         if (value && !text && tagName === 'INPUT') {
-//           text = value;
-//           value = '';
-//         }
-//       } else {
-//         throw { message: `Can't find selector ${selector}` };
-//       }
-
-//       await log({
-//         text: `Get text: '${text}' from selector: '${selector}'`,
-//         screenshot: screenshot,
-//         fullpage: false,
-//         element: element,
-//         level: 'debug',
-//         levelIndent: levelIndent + 1,
-//       });
-
-//       return { text, value };
-//     } catch (error) {
-//       error.testType = 'atom';
-//       error.testArgs = args;
-//       error.testVars = { selector, screenshot, element, attr, text, value, selectorSelect, elementSelect, tagName };
-//       throw error;
-//     }
-//   },
-// };
--- selectors/countSelectors.js
+++ selectors/countSelectors.js
@@ -2,49 +2,21 @@ const instance = new (require('@puppedo/atoms-core'))();
 module.exports = { runTest: instance.runTest.bind(instance) };
 
 // WRITE YOUR LOGIC BELLOW
-instance.atomRun = async function() {
+instance.atomRun = async function () {
   const { selector } = this.selectors;
   const { timeDelay } = this.options;
+
+  let elements = [];
 
   if (timeDelay) {
     await this.page.waitFor(timeDelay);
   }
 
   await this.log({ text: `Counting selectors: '${selector}'` });
-  const elements = await this.getElement(selector, true);
+  try {
+    elements = await this.getElement(selector, true);
+  } catch (error) {}
   await this.log({ text: `Selectors '${selector}' found '${elements.length}'` });
 
   return { count: elements.length };
 };
-
-// module.exports = {
-//   runTest: async function(args) {
-//     const { page, selectors, options, log, helper, levelIndent } = args;
-//     const selector = helper.anyGet(selectors, 'selector');
-
-//     const timeDelay = helper.anyGet(options, 'timeDelay');
-//     if (timeDelay) {
-//       await page.waitFor(timeDelay);
-//     }
-
-//     await log({
-//       text: `Подсчет количества селекторов = ${selectors.selector}`,
-//       screenshot: false,
-//       level: 'raw',
-//       levelIndent: levelIndent + 1,
-//     });
-
-//     const elements = await helper.getElement(page, selector, true);
-
-//     await log({
-//       text: `Селекторов ${selectors.selector} найдено = ${elements.length}`,
-//       screenshot: false,
-//       level: 'raw',
-//       levelIndent: levelIndent + 1,
-//     });
-
-//     return {
-//       count: elements.length,
-//     };
-//   },
-// };
