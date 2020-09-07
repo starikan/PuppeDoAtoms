45d51f423d8b17b634458e13b94b0f2c0f0bc33e
Sergey Starodubov <4analit@gmail.com>
2020-09-07 21:24:03 +0400

    saveToFile

---
+++ files/saveToFile.js
@@ -0,0 +1,25 @@
+const instance = new (require('@puppedo/atoms-core'))();
+module.exports = { runTest: instance.runTest.bind(instance) };
+
+// WRITE YOUR LOGIC BELLOW
+instance.atomRun = async function () {
+  const fs = require('fs');
+  const path = require('path');
+
+  const { object, fileName, folder } = this.data;
+  const { folderFull, folderLatestFull } = this.logOptions.output || {};
+
+  const json = JSON.stringify(object, null, 2)
+
+  if (folder) {
+    if (!fs.existsSync(folder)) {
+      fs.mkdirSync(folder);
+    }
+    fs.writeFileSync(path.join(folder, fileName), json);
+    await this.log({ text: `Write file: ${path.join(folder, fileName)}` });
+  }
+
+  fs.writeFileSync(path.join(folderFull, fileName), json);
+  fs.writeFileSync(path.join(folderLatestFull, fileName), json);
+  await this.log({ text: `Write file: ${path.join(folderFull, fileName)}` });
+};
