e6d44ccb5f980bc2c139fc430faefc55bd24b27a
Sergey Starodubov <4analit@gmail.com>
2020-09-08 17:22:21 +0400

    checkSelector update

--- checks/checkSelectorRaw.js
+++ checks/checkSelectorRaw.js
@@ -6,11 +6,11 @@ instance.atomRun = async function() {
   const { selector } = this.selectors;
   const element = await this.getElement(selector);
 
-  if (element) {
-    await this.log({ text: `Check selector exist: '${selector}'`, element });
-  } else {
+  if (!element || (Array.isArray(element) && !element.length)) {
     await this.log({ text: `Check selector NOT exist: '${selector}'` });
+    return { exists: false };
+  } else {
+    await this.log({ text: `Check selector exist: '${selector}'`, element });
+    return { exists: true };
   }
-
-  return { exists: !!element };
 };
