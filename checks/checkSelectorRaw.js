05c992e64efd9ab31f707b3aa0ab8086ff69fb9d
Sergey Starodubov <4analit@gmail.com>
2020-09-29 13:49:23 +0400

    checkSelector selectorNumber

--- checks/checkSelectorRaw.js
+++ checks/checkSelectorRaw.js
@@ -1,6 +1,8 @@
 module.exports = async function atomRun() {
   const { selector } = this.selectors;
-  const element = await this.getElement(selector);
+  const { selectorNumber = 0 } = this.data;
+
+  const element = ((await this.getElement(selector, true)) || [])[selectorNumber];
 
   if (!element || (Array.isArray(element) && !element.length)) {
     await this.log({ text: `Check selector NOT exist: '${selector}'` });
