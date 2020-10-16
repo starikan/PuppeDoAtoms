4e30f2e3636346372c68d22a77f3efedc07cc2e3
Sergey Starodubov <4analit@gmail.com>
2020-10-16 14:26:14 +0400

    envSwitch refactoring

--- main/envSwitch.js
+++ main/envSwitch.js
@@ -1,5 +1,5 @@
 module.exports = async function atomRun() {
   const { envName } = this.data;
-  await this.envs.setEnv(envName);
   await this.log({ text: `Environment switch on '${envName}'` });
+  await this.envs.setEnv(envName);
 };
