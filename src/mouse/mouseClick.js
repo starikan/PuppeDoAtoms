9bc1db58d5e6a42244277c035c92d3540fac2348
Sergey Starodubov <4analit@gmail.com>
2020-11-09 20:09:44 +0400

    mouseClick

--- src/mouse/mouseClick.js
+++ src/mouse/mouseClick.js
@@ -1,33 +1,8 @@
 module.exports = async function atomRun() {
-  const { selector } = this.selectors;
-  const element = await this.getElement(selector);
   const { X, Y } = this.data;
 
   const mouse = this.page.mouse;
   await mouse.click(X, Y);
 
-  await this.log({ text: `Mouse click in coords: X = ${X}, Y = ${Y}`, element });
+  await this.log({ text: `Mouse click in coords: X = '${X}', Y = '${Y}'` });
 };
-
-// module.exports = {
-//   runTest: async function(args) {
-//     const { page, selectors, data, log, options, helper, levelIndent, _ } = args;
-//     const selector = helper.anyGet(selectors, 'selector');
-//     const screenshot = _.get(options, 'screenshot', false);
-//     const element = await helper.getElement(page, selector);
-//     const X = data.X;
-//     const Y = data.Y;
-
-//     const mouse = page.mouse;
-//     await mouse.click(X, Y);
-
-//     await log({
-//       text: `Мышь кликнула по странице в координатах X = ${X}, Y = ${Y}`,
-//       screenshot: screenshot,
-//       fullpage: false,
-//       element: element,
-//       level: 'raw',
-//       levelIndent: levelIndent + 1,
-//     });
-//   },
-// };
