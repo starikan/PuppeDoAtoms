e869389872d7df3943362040cee1266e5dbf371e
Sergey Starodubov <4analit@gmail.com>
2020-11-09 11:37:17 +0400

    Remove lodash

--- src/modify/setPageName.js
+++ src/modify/setPageName.js
@@ -4,34 +4,10 @@ module.exports = async function atomRun() {
   const allPages = await this.browser.pages();
   if (['last', -1].includes(position)) {
     this.env.set(`state.pages.${name}`, allPages[allPages.length - 1]);
-  } else if (this._.isNumber(position)) {
-    this.env.set(`state.pages.${name}`, allPages[position]);
+  } else {
+    const numberResolved = parseInt(position) || 0;
+    this.env.set(`state.pages.${name}`, allPages[numberResolved]);
   }
 
   await this.log({ text: `Page set name: '${name}'` });
 };
-
-// module.exports = {
-//   runTest: async function(args) {
-//     const { env, data, browser, log, options, levelIndent, _ } = args;
-//     const screenshot = _.get(options, 'screenshot', false);
-
-//     const position = _.get(data, 'position');
-//     const name = _.get(data, 'name');
-
-//     const allPages = await browser.pages();
-//     if (['last', -1].includes(position)) {
-//       console.log(env.set(`state.pages.${name}`, allPages[allPages.length - 1]));
-//     } else if (_.isNumber(position)) {
-//       env.set(`state.pages.${name}`, allPages[position]);
-//     }
-
-//     await log({
-//       text: `Переключились на страницу ${name}`,
-//       screenshot: screenshot,
-//       fullpage: true,
-//       level: 'raw',
-//       levelIndent: levelIndent + 1,
-//     });
-//   },
-// };
--- src/mouse/mouseMove.js
+++ src/mouse/mouseMove.js
@@ -1,56 +1,20 @@
 module.exports = async function atomRun() {
-  const { X, Y, dX, dY } = this.data;
+  const X = this.data.X || 0;
+  const Y = this.data.Y || 0;
+  const dX = this.data.dX || 0;
+  const dY = this.data.dY || 0;
 
   const mouse = this.page.mouse;
   const mouseX = mouse._x;
   const mouseY = mouse._y;
 
-  if (!this._.isUndefined(dX) && !this._.isUndefined(dY)) {
+  if (dX && dY) {
     await mouse.move(mouseX + dX, mouseY + dY, { steps: 50 });
     await this.log({ text: `Mouse move on range dX = ${dX}, dY = ${dY}` });
-  } else if (!this._.isUndefined(X) && !this._.isUndefined(Y)) {
+  } else if (X && Y) {
     await mouse.move(X, Y);
     await this.log({ text: `Mouse move on position X = ${X}, Y = ${Y}` });
   } else {
     throw { message: 'Bad data. Must be X-Y or dX-dY.' };
   }
 };
-
-// module.exports = {
-//   runTest: async function(args) {
-//     const { page, data, log, options, levelIndent, _ } = args;
-//     const screenshot = _.get(options, 'screenshot', false);
-//     const X = _.get(data, 'X');
-//     const Y = _.get(data, 'Y');
-//     const dX = _.get(data, 'dX');
-//     const dY = _.get(data, 'dY');
-
-//     const mouse = page.mouse;
-//     const mouseX = mouse._x;
-//     const mouseY = mouse._y;
-
-//     if (!_.isUndefined(dX) && !_.isUndefined(dY)) {
-//       await mouse.move(mouseX + dX, mouseY + dY, { steps: 50 });
-//       await log({
-//         text: `Мышь перемещена на расстоние X = ${dX}, Y = ${dY}`,
-//         screenshot: screenshot,
-//         fullpage: false,
-//         level: 'raw',
-//         levelIndent: levelIndent + 1,
-//       });
-//     } else if (!_.isUndefined(X) && !_.isUndefined(Y)) {
-//       await mouse.move(X, Y);
-//       await log({
-//         text: `Мышь перемещена на позицию X = ${X}, Y = ${Y}`,
-//         screenshot: screenshot,
-//         fullpage: false,
-//         level: 'raw',
-//         levelIndent: levelIndent + 1,
-//       });
-//     } else {
-//       throw {
-//         message: 'Не передан нужный набор параметров. Должно быть сочетания X-Y или dX-dY',
-//       };
-//     }
-//   },
-// };
