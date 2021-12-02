0b75836b2a331c3c22ba05c4ddfe92b0d14c07e8
Sergey Starodubov <4analit@gmail.com>
2021-12-02 13:12:18 +0400

    selectSelector

--- src/selectors/selectSelector.js
+++ src/selectors/selectSelector.js
@@ -3,7 +3,7 @@ module.exports = async function atomRun() {
   const { option } = this.data;
   const element = await this.getElement(selector);
 
-  await this.page.select(selector, option);
+  await element.select(option);
 
   await this.log({ text: `Selector select: '${selector}'`, element });
 };
