af775b18c3846b2c007457ab4d5b8e878792776b
Sergey Starodubov <4analit@gmail.com>
2022-11-09 00:14:30 +0400

    env -> runner

--- src/modify/setPageName.js
+++ src/modify/setPageName.js
@@ -1,12 +1,13 @@
+// TODO: 2022-11-09 S.Starodubov todo
 module.exports = async function atomRun() {
   const { position, name } = this.data;
 
   const allPages = await this.browser.pages();
   if (['last', -1].includes(position)) {
-    this.env.set(`state.pages.${name}`, allPages[allPages.length - 1]);
+    this.runner.set(`state.pages.${name}`, allPages[allPages.length - 1]);
   } else {
     const numberResolved = parseInt(position) || 0;
-    this.env.set(`state.pages.${name}`, allPages[numberResolved]);
+    this.runner.set(`state.pages.${name}`, allPages[numberResolved]);
   }
 
   await this.log({ text: `Page set name: '${name}'` });
